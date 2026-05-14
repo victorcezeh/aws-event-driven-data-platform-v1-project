ec395c3eda9b7b687a6ac0d8add84149de35168d
victorcezeh <ezeh_victor@yahoo.com>
2026-05-14 23:39:50 +0100

    Local dev done and dusted for real now. On to infra work

--- config/settings.py
+++ config/settings.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 def get_config():
     load_dotenv()
     return {
-        "aws_access_key":          os.getenv("AWS_ACCESS_KEY"),
-        "aws_secret_access_key":   os.getenv("AWS_SECRET_ACCESS_KEY_ID"),
+        "aws_access_key":          os.getenv("AWS_ACCESS_KEY_ID"),
+        "aws_secret_access_key":   os.getenv("AWS_SECRET_ACCESS_KEY"),
         "bucket_name":             os.getenv("AWS_BUCKET_NAME"),
         "bucket_region":           os.getenv("AWS_REGION"),
+        "key":                     os.getenv("AWS_S3_OBJECT_KEY"),
         "url":                     f"{os.getenv('BASE_URL')}{os.getenv('END_POINT')}",
-        "key":                     os.getenv("KEY"),
         "log_file":                os.getenv("LOG_FILE_PATH"),
         "processed_csv_file_path": os.getenv("PROCESSED_CSV_FILE_PATH"),
         "raw_json_file_path":      os.getenv("RAW_JSON_FILE_PATH"),
